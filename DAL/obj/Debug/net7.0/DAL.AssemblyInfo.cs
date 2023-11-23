6da1166765c51b71cf97872b19f1551d30a87cc5
TuKi2901 <huynhtrantuankiet2901@gmail.com>
2023-11-23 17:01:57 +0700

    a

--- DAL/obj/Debug/net7.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net7.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c811121a900c57eba64890abfeefb306d271e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
