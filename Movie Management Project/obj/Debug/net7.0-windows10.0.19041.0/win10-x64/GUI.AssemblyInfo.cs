b8fd1a96c4e12f68c2645b1d7f19790a2aac6374
TuKi2901 <huynhtrantuankiet2901@gmail.com>
2023-11-23 16:54:12 +0700

    a
    a

--- Movie Management Project/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/GUI.AssemblyInfo.cs
+++ Movie Management Project/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/GUI.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ca539d52d32969e589c9d713c3fedfd56d385184")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d4c811121a900c57eba64890abfeefb306d271e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
