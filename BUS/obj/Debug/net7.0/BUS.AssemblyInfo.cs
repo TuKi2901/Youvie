0570fd9724d2d39af498a2dde18d8d2068a01aee
TuanMinh1011 <129381372+TuanMinh1011@users.noreply.github.com>
2023-11-28 18:28:05 +0700

    a

--- BUS/obj/Debug/net7.0/BUS.AssemblyInfo.cs
+++ BUS/obj/Debug/net7.0/BUS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BUS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196d953e4e3fa0814063ef364ed785e4d7d8feb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BUS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BUS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
