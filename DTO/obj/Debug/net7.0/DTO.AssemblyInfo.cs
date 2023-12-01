6838f1406db81bb0333031a670065b344da83fe8
TuKi2901 <huynhtrantuankiet2901@gmail.com>
2023-12-01 18:43:34 +0700

    Complete Forgot Password

--- DTO/obj/Debug/net7.0/DTO.AssemblyInfo.cs
+++ DTO/obj/Debug/net7.0/DTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd93809d18229f642ddb51bc2db8c31391af823b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
