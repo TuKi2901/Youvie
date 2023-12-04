e3fc05b686a5fbcd57f88c4e2539edee8f64c400
TuKi2901 <huynhtrantuankiet2901@gmail.com>
2023-12-04 15:27:20 +0700

    a
    a

--- DTO/obj/Debug/net7.0/DTO.AssemblyInfo.cs
+++ DTO/obj/Debug/net7.0/DTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3257671a6eef6729fafec0bbd56095d53bfbd4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
